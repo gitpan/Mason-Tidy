7fd4731688ce22962b7112d9ac57fec41ea8e30c
Jonathan Swartz <swartz@pobox.com>
2012-09-12 18:03:58 +0000

    Import of JSWARTZ/Mason-Tidy-0.01 from CPAN.
    gitpan-cpan-distribution: Mason-Tidy
    gitpan-cpan-version:      0.01
    gitpan-cpan-path:         JSWARTZ/Mason-Tidy-0.01.tar.gz
    gitpan-cpan-author:       JSWARTZ
    gitpan-cpan-maturity:     released

---
+++ lib/Mason/Tidy.pm
@@ -0,0 +1,346 @@
+package Mason::Tidy;
+BEGIN {
+  $Mason::Tidy::VERSION = '0.01';
+}
+use File::Slurp;
+use Method::Signatures::Simple;
+use Moo;
+use Perl::Tidy qw();
+use strict;
+use warnings;
+
+my $marker_count = 0;
+
+# Public
+has 'indent_perl_block'   => ( is => 'ro', default => sub { 2 } );
+has 'perltidy_argv'       => ( is => 'ro', default => sub { '' } );
+has 'perltidy_block_argv' => ( is => 'ro', default => sub { '' } );
+has 'perltidy_line_argv'  => ( is => 'ro', default => sub { '-i=2' } );
+has 'perltidy_tag_argv'   => ( is => 'ro', default => sub { '' } );
+
+# Private
+has '_is_mixed_block'   => ( is => 'lazy' );
+has '_is_perl_block'    => ( is => 'lazy' );
+has '_marker_prefix'    => ( is => 'ro', default => sub { '__masontidy__' } );
+has '_open_block_regex' => ( is => 'lazy' );
+has '_subst_tag_regex'  => ( is => 'lazy' );
+
+method _build__is_mixed_block () {
+    return { map { ( $_, 1 ) } $self->mixed_block_names };
+}
+
+method _build__is_perl_block () {
+    return { map { ( $_, 1 ) } $self->perl_block_names };
+}
+
+method _build__open_block_regex () {
+    my $re = '^\s*<%(' . join( '|', $self->block_names ) . ')(\s+\w+)?>\s*$';
+    return qr/$re/;
+}
+
+method _build__subst_tag_regex () {
+    my $re = '<%(?!' . join( '|', $self->block_names ) . ')(.*?)%>';
+    return qr/$re/;
+}
+
+method block_names () {
+    return
+      qw(after args around attr augment before class cleanup def doc filter flags init method once override perl shared text);
+}
+
+method perl_block_names () {
+    return qw(class init once perl shared);
+}
+
+method mixed_block_names () {
+    return qw(after augment around before def method override);
+}
+
+method tidy ($source) {
+    return $self->tidy_method($source);
+}
+
+method tidy_method ($source) {
+    my @lines            = split( /\n/, $source );
+    my @elements         = ();
+    my $add_element      = sub { push( @elements, [@_] ) };
+    my $open_block_regex = $self->_open_block_regex;
+
+    my $last_line = scalar(@lines) - 1;
+    for ( my $cur_line = 0 ; $cur_line <= $last_line ; $cur_line++ ) {
+        my $line = $lines[$cur_line];
+
+        # Begin Mason 2 filter invocation
+        #
+        if ( $line =~ /^%\s*(.*)\{\{\s*/ ) {
+            $add_element->( 'perl_line', "% given (__filter($1)) {" );
+            next;
+        }
+
+        # End Mason 2 filter invocation
+        #
+        if ( $line =~ /^%\s*\}\}\s*/ ) {
+            $add_element->( 'perl_line', "% } # __end filter" );
+            next;
+        }
+
+        # %-line
+        #
+        if ( $line =~ /^%/ ) {
+            $add_element->( 'perl_line', $line );
+            next;
+        }
+
+        # Block start and end, each on their own line
+        #
+        if ( my ( $block_type, $block_args ) = ( $line =~ /$open_block_regex/m ) ) {
+            if ( my $end_line =
+                $self->capture_block( \@lines, $block_type, $cur_line + 1, $last_line ) )
+            {
+                my $block_contents = join( "\n", @lines[ $cur_line + 1 .. $end_line - 1 ] );
+                $block_contents = join( "\n",
+                    $lines[$cur_line],
+                    grep { /\S/ } $self->handle_block( $block_type, $block_args, $block_contents ),
+                    $lines[$end_line] );
+                $add_element->( 'block', $block_contents );
+                $cur_line = $end_line;
+                next;
+            }
+        }
+
+        # Everything else goes untouched
+        #
+        $add_element->( 'text_line', $line );
+    }
+
+    # Create content from elements with non-perl lines as comments; perltidy;
+    # reassemble list of elements from tidied perl blocks and replaced elements
+    #
+    my $untidied_perl = join(
+        "\n",
+        map {
+            $_->[0] eq 'perl_line'
+              ? trim( substr( $_->[1], 1 ) )
+              : $self->replace_with_perl_comment($_)
+        } @elements
+    );
+    $self->perltidy(
+        source      => \$untidied_perl,
+        destination => \my $tidied_perl,
+        argv        => $self->perltidy_line_argv . " -fnl -fbl",
+    );
+
+    @elements = ();
+    foreach my $line ( split( /\n/, $tidied_perl ) ) {
+        if ( my $marker = $self->marker_in_line($line) ) {
+            $add_element->( @{ $self->restore($marker) } );
+        }
+        else {
+            # Convert back filter invocation
+            #
+            $line =~ s/given\s*\(\s*__filter\s*\(\s*(.*?)\s*\)\s*\)\s*\{/$1 \{\{/;
+            $line =~ s/\}\s*\#\s*__end filter/\}\}/;
+            $add_element->( 'perl_line', "% " . $line );
+        }
+    }
+    my $final = join( "\n", map { $_->[1] } @elements );
+
+    # Tidy Perl in <% %> tags
+    #
+    my $subst_tag_regex = $self->_subst_tag_regex;
+    $final =~ s/$subst_tag_regex/"<% " . $self->tidy_subst_expr($1) . " %>"/ge;
+
+    # Tidy Perl in <% %> tags
+    #
+    $final =~ s/<&(.*?)&>/"<& " . $self->tidy_compcall_expr($1) . " &>"/ge;
+
+    return $final;
+}
+
+method tidy_subst_expr ($expr) {
+    $self->perltidy(
+        source      => \$expr,
+        destination => \my $tidied_expr,
+        argv        => $self->perltidy_tag_argv,
+    );
+    return trim($tidied_expr);
+}
+
+method tidy_compcall_expr ($expr) {
+    my $path;
+    if ( ($path) = ( $expr =~ /^(\s*[\w\/\.][^,]+)/ ) ) {
+        substr( $expr, 0, length($path) ) = "'$path'";
+    }
+    $self->perltidy(
+        source      => \$expr,
+        destination => \my $tidied_expr,
+        argv        => $self->perltidy_tag_argv,
+    );
+    if ($path) {
+        substr( $tidied_expr, 0, length($path) + 2 ) = $path;
+    }
+    return trim($tidied_expr);
+}
+
+method capture_block ($lines, $block_type, $cur_line, $last_line) {
+    foreach my $this_line ( $cur_line .. $last_line ) {
+        if ( $lines->[$this_line] =~ m{</%$block_type>} ) {
+            return $this_line;
+        }
+    }
+    return undef;
+}
+
+method handle_block ($block_type, $block_args, $block_contents) {
+    if ( $self->_is_perl_block->{$block_type}
+        || ( $block_type eq 'filter' && !defined($block_args) ) )
+    {
+        $block_contents = trim_lines($block_contents);
+        $self->perltidy(
+            source      => \$block_contents,
+            destination => \my $tidied_block_contents,
+            argv        => $self->perltidy_block_argv
+        );
+        $block_contents = trim($tidied_block_contents);
+        my $spacer = scalar( ' ' x $self->indent_perl_block );
+        $block_contents =~ s/^/$spacer/mg;
+    }
+    elsif ( $self->_is_mixed_block->{$block_type}
+        || ( $block_type eq 'filter' && defined($block_args) ) )
+    {
+        $block_contents = $self->tidy_method($block_contents);
+    }
+    return $block_contents;
+}
+
+method replace_with_perl_comment ($obj) {
+    return "# " . $self->replace_with_marker($obj);
+}
+
+method replace_with_marker ($obj) {
+    my $marker = join( "_", $self->_marker_prefix, $marker_count++ );
+    $self->{markers}->{$marker} = $obj;
+    return $marker;
+}
+
+method marker_in_line ($line) {
+    my $marker_prefix = $self->_marker_prefix;
+    if ( my ($marker) = ( $line =~ /(${marker_prefix}_\d+)/ ) ) {
+        return $marker;
+    }
+    return undef;
+}
+
+method restore ($marker) {
+    return $self->{markers}->{$marker};
+}
+
+method perltidy (%params) {
+    $params{argv} .= ' ' . $self->perltidy_argv;
+    my $errorfile;
+    Perl::Tidy::perltidy(
+        prefilter  => \&perltidy_prefilter,
+        postfilter => \&perltidy_postfilter,
+        errorfile  => \$errorfile,
+        %params
+    );
+    die $errorfile if $errorfile;
+}
+
+func perltidy_prefilter ($buf) {
+    $buf =~ s/\$\./\$__SELF__->/g;
+    return $buf;
+}
+
+func perltidy_postfilter ($buf) {
+    $buf =~ s/\$__SELF__->/\$\./g;
+    $buf =~ s/ *\{ *\{/ \{\{/g;
+    $buf =~ s/ *\} *\}/\}\}/g;
+    return $buf;
+}
+
+func trim ($str) {
+    for ($str) { s/^\s+//; s/\s+$// }
+    return $str;
+}
+
+func trim_lines ($str) {
+    for ($str) { s/^\s+//m; s/\s+$//m }
+    return $str;
+}
+
+1;
+
+
+
+=pod
+
+=head1 NAME
+
+Mason::Tidy - Engine for masontidy
+
+=head1 VERSION
+
+version 0.01
+
+=head1 SYNOPSIS
+
+    use Mason::Tidy;
+
+    my $mc = Mason::Tidy->new();
+    my $dest = $mc->tidy($source);
+
+=head1 DESCRIPTION
+
+This is the engine used by L<masontidy|masontidy> - read that first to get an
+overview.
+
+You can call this API from your own program instead of executing C<masontidy>.
+
+=head1 CONSTRUCTOR PARAMETERS
+
+=over
+
+=item indent_perl_block
+
+=item perltidy_argv
+
+=item perltidy_block_argv
+
+=item perltidy_line_argv
+
+=item perltidy_subst_argv
+
+These options are the same as the equivalent C<masontidy> command-line options,
+replacing dashes with underscore (e.g. the C<--indent-per-block> option becomes
+C<indent_perl_block> here).
+
+=back
+
+=head1 METHODS
+
+=over
+
+=item tidy ($source)
+
+Tidy component source I<$source> and return the tidied result. Throw fatal
+error if source cannot be tidied (e.g. invalid syntax).
+
+=back
+
+=head1 AUTHOR
+
+Jonathan Swartz <swartz@pobox.com>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2011 by Jonathan Swartz.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+=cut
+
+
+__END__
+
---
+++ lib/Mason/Tidy/t/Basic.pm
@@ -0,0 +1,377 @@
+package Mason::Tidy::t::Basic;
+BEGIN {
+  $Mason::Tidy::t::Basic::VERSION = '0.01';
+}
+use Mason::Tidy;
+use Test::More;
+use strict;
+use warnings;
+use base qw(Test::Class);
+
+sub tidy {
+    my %params  = @_;
+    my $source  = $params{source} or die "source required";
+    my $desc    = $params{desc} or die "desc required";
+    my $options = $params{options} || {};
+
+    my $mt = Mason::Tidy->new( %$options, perltidy_argv => '' );
+    my $dest = eval { $mt->tidy($source) };
+    my $err = $@;
+    if ( my $expect_error = $params{expect_error} ) {
+        like( $err, $expect_error, "got error - $desc" );
+        is( $dest, undef, "no dest returned - $desc" );
+    }
+    else {
+        is( $err, '', "no error - $desc" );
+        is( trim($dest), trim( $params{expect} ), "expected content - $desc" );
+    }
+}
+
+sub trim {
+    my $str = $_[0];
+    return undef if !defined($str);
+    for ($str) { s/^\s+//; s/\s+$// }
+    return $str;
+}
+
+sub test_perl_sections : Tests {
+    tidy(
+        desc   => 'init section',
+        source => '
+<%init>
+if($foo  )   {  
+my @ids = (1,2);
+    }  
+</%init>
+',
+        expect => '
+<%init>
+  if ($foo) {
+      my @ids = ( 1, 2 );
+  }
+</%init>
+'
+    );
+
+    tidy(
+        desc   => 'empty init section',
+        source => '
+<%init>
+
+</%init>
+',
+        expect => '
+<%init>
+</%init>
+'
+    );
+}
+
+sub test_mixed_sections : Tests {
+    tidy(
+        desc   => 'method',
+        source => '
+<%method foo>
+%if (  $foo) {
+content
+%}
+</%method>
+',
+        expect => '
+<%method foo>
+% if ($foo) {
+content
+% }
+</%method>
+'
+    );
+
+    tidy(
+        desc   => 'empty method',
+        source => '
+<%method foo>
+</%method>
+',
+        expect => '
+<%method foo>
+</%method>
+'
+    );
+}
+
+sub test_perl_lines : Tests {
+    tidy(
+        desc   => 'perl lines',
+        source => '
+%if($foo  )   {  
+%my @ids = (1,2);
+%    }  
+',
+        expect => '
+% if ($foo) {
+%   my @ids = ( 1, 2 );
+% }
+'
+    );
+}
+
+sub test_tags : Tests {
+    tidy(
+        desc   => 'subst tag',
+        source => '<%$x%> text <%foo(5,6)%>',
+        expect => '<% $x %> text <% foo( 5, 6 ) %>',
+    );
+    tidy(
+        desc   => 'comp call tag',
+        source => '<&/foo/bar,a=>5,b=>6&> text <&  $comp_path, str=>"foo"&>',
+        expect =>
+          '<& /foo/bar, a => 5, b => 6 &> text <& $comp_path, str => "foo" &>',
+    );
+}
+
+sub test_filter_invoke : Tests {
+    tidy(
+        desc   => 'filter invoke',
+        source => '
+%$.Trim(3,17) {{
+%sub {uc($_[0]  )} {{
+%$.Fobernate() {{
+   This string will be trimmed, uppercased
+   and fobernated
+% }}
+%}}
+%   }}
+',
+        expect => '
+% $.Trim( 3, 17 ) {{
+%   sub { uc( $_[0] ) } {{
+%     $.Fobernate() {{
+   This string will be trimmed, uppercased
+   and fobernated
+%     }}
+%   }}
+% }}
+'
+    );
+}
+
+sub test_filter_decl : Tests {
+    tidy(
+        desc   => 'Mason 1 filter declaration (no arg)',
+        source => '
+Hi
+
+<%filter>
+if (/abc/) {
+s/abc/def/;
+}
+</%filter>
+',
+        expect => '
+Hi
+
+<%filter>
+  if (/abc/) {
+      s/abc/def/;
+  }
+</%filter>
+'
+    );
+
+    tidy(
+        desc   => 'Mason 2 filter declaration (w/ arg)',
+        source => '
+<%filter Item ($class)>
+<li class="<%$class%>">
+%if (my $x = $yield->()) {
+<% $x %>
+%}
+</li>
+</%filter>
+',
+        expect => '
+<%filter Item ($class)>
+<li class="<% $class %>">
+% if ( my $x = $yield->() ) {
+<% $x %>
+% }
+</li>
+</%filter>
+'
+    );
+}
+
+sub test_perltidy_argv : Tests {
+    tidy(
+        desc   => 'default indent 2',
+        source => '
+% if ($foo) {
+% if ($bar) {
+% baz();
+% }
+% }
+',
+        expect => '
+% if ($foo) {
+%   if ($bar) {
+%     baz();
+%   }
+% }
+'
+    );
+    tidy(
+        desc    => 'perltidy_line_argv = -i=2',
+        options => { perltidy_line_argv => '-i=2' },
+        source  => '
+% if ($foo) {
+% if ($bar) {
+% baz();
+% }
+% }
+',
+        expect => '
+% if ($foo) {
+%   if ($bar) {
+%     baz();
+%   }
+% }
+'
+    );
+}
+
+sub test_single_line_block : Tests {
+    tidy(
+        desc   => 'indent_perl_block 0',
+        source => '
+<%perl>my $foo = 5;</%perl>
+',
+        expect => '
+<%perl>my $foo = 5;</%perl>
+'
+    );
+}
+
+sub test_indent_perl_block : Tests {
+    my $source = '
+<%perl>
+    if ($foo) {
+$bar = 6;
+  }
+</%perl>
+';
+    tidy(
+        desc    => 'indent_perl_block 0',
+        options => { indent_perl_block => 0 },
+        source  => $source,
+        expect  => '
+<%perl>
+if ($foo) {
+    $bar = 6;
+}
+</%perl>
+'
+    );
+    tidy(
+        desc   => 'indent_perl_block 2 (default)',
+        source => $source,
+        expect => '
+<%perl>
+  if ($foo) {
+      $bar = 6;
+  }
+</%perl>
+'
+    );
+
+    tidy(
+        desc   => 'indent_perl_block 4',
+        source => $source,
+        expect => '
+<%perl>
+  if ($foo) {
+      $bar = 6;
+  }
+</%perl>
+'
+    );
+}
+
+sub test_errors : Tests {
+    tidy(
+        desc         => 'syntax error',
+        source       => '% if ($foo) {',
+        expect_error => qr/final indentation level/,
+    );
+}
+
+sub test_comprehensive : Tests {
+    tidy(
+        desc   => 'comprehensive',
+        source => '
+some text
+
+% if ( $contents || $allow_empty ) {
+  <ul>
+% foreach my $line (@lines) {
+<%perl>
+dothis();
+andthat();
+</%perl>
+  <li>
+      <%2+(3-4)*6%>
+  </li>
+  <li><%  foo($.bar,$.baz,  $.bleah)   %></li>
+% }
+  </ul>
+% }
+
+%  $.Filter(3,2) {{  
+some filtered text
+%}}
+
+<&footer,color=>"blue",height  =>  3&>
+
+<%method foo>
+%if(defined($bar)) {
+% if  ( $write_list) {
+even more text
+%}
+% }
+</%method>
+',
+        expect => '
+some text
+
+% if ( $contents || $allow_empty ) {
+  <ul>
+%   foreach my $line (@lines) {
+<%perl>
+  dothis();
+  andthat();
+</%perl>
+  <li>
+      <% 2 + ( 3 - 4 ) * 6 %>
+  </li>
+  <li><% foo( $.bar, $.baz, $.bleah ) %></li>
+%   }
+  </ul>
+% }
+
+% $.Filter( 3, 2 ) {{
+some filtered text
+% }}
+
+<& footer, color => "blue", height => 3 &>
+
+<%method foo>
+% if ( defined($bar) ) {
+%   if ($write_list) {
+even more text
+%   }
+% }
+</%method>
+'
+    );
+}
+
+1;
